adf9954f5d336bd36a48883bb9104ba1524ea0af
zgeniuscoders <zgeniuscoders@gmail.com>
2025-05-21 16:47:31 +0100

    core: creation des fichier pour injecter les variable global comme l'url du back etc.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {provideHttpClient} from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BASE_URL } from './app.tokens';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: BASE_URL, useValue: 'http://127.0.0.1:8000/api' },
+  ],
 };
