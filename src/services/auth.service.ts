dc27f84f4619430019703f2ebae014cb2c004955
zgeniusecoders <zgeniusecoders@gmail.com>
2025-05-27 16:27:56 +0100

    feat: creation du service d'authentification

---
+++ src/services/auth.service.ts
@@ -0,0 +1,23 @@
+import {HttpClient} from '@angular/common/http';
+import {inject, Injectable} from '@angular/core';
+import {BASE_URL} from '../app/app.tokens';
+import {ResponseInterface} from '../models/response.model';
+import {User} from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServices {
+
+  http: HttpClient = inject(HttpClient)
+  baseUrl = inject(BASE_URL)
+
+  async login(data: { cuid: string, password: string }) {
+    return this.http.post<ResponseInterface<User>>(`${this.baseUrl}/login`, data)
+  }
+
+  async logout() {
+    return this.http.post(`${this.baseUrl}/logout`, {})
+  }
+
+}
