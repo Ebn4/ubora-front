1793577c6331bd1847b3fa4b2938e38496332e5e
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-02 16:27:16 +0100

    feat: ajout du middleware d'authentification

---
+++ src/app/middlewares/auth.guard.ts
@@ -0,0 +1,15 @@
+import {inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {LocalStorageService} from '../services/local-storage.service';
+
+export const AuthGuard = async () => {
+  const localStorageService = inject(LocalStorageService)
+  const router = inject(Router)
+
+  if (!localStorageService.getData("token")) {
+    await router.navigateByUrl('/login')
+    return false
+  }
+
+  return true
+}
