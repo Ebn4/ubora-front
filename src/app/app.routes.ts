94cb7acd84c754a331e8016d5d042444a01bf069
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-02 16:23:16 +0100

    refactor: deplacement des dossier dans le dossier app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,6 @@ export const routes: Routes = [
   {
     path: 'login',
     title: 'Connexion',
-    loadComponent: () => import('../pages/login/login.component').then(r => r.LoginComponent)
+    loadComponent: () => import('./pages/login/login.component').then(r => r.LoginComponent)
   }
 ];
