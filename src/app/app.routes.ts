7005efc65e787de64ba6322d9d4f90a27e5fa530
Dibakala-Muke <dibakala90@gmail.com>
2025-06-02 14:29:33 +0100

    Configuration des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { PeriodComponent } from './pages/period/period.component';
+import { PeriodSingleComponent } from './pages/period-single/period-single.component';
+import { ImportComponent } from './pages/import/import.component';
+import { CandidaciesComponent } from './pages/candidacies/candidacies.component';
+import { CriteriaComponent } from './pages/criteria/criteria.component';
+import { SidebarComponent } from './pages/layout/sidebar/sidebar.component';
+import { CandidacySingleComponent } from './pages/candidacy-single/candidacy-single.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SidebarComponent,
+    children: [
+      {path: '', redirectTo: 'criteria', pathMatch:'full'},
+      {path: 'period', component:PeriodComponent},
+      {path: 'criteria', component:CriteriaComponent},
+      {path: 'period-single/:id', component:PeriodSingleComponent},
+      {path: 'import', component:ImportComponent},
+      {path: 'candidacy', component:CandidaciesComponent},
+      {path: 'candidacy-single/:id', component: CandidacySingleComponent}
+    ]
+  }
+];
