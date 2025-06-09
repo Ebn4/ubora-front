6286012ff8e77a34885522740ad853c4a569db90
Dibakala-Muke <dibakala90@gmail.com>
2025-06-09 10:41:05 +0100

    Ajout d'une nouvelle fonctionnalité pour les candidats d'une période

--- src/app/pages/period-single/period-candidacy/period-candidacy.component.ts
+++ src/app/pages/period-single/period-candidacy/period-candidacy.component.ts
@@ -1,38 +1,61 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgFor } from '@angular/common';
 import { Candidacy } from '../../../models/candidacy';
 import { CandidacyService } from '../../../services/candidacy.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BaseListWidget } from '../../../widgets/base-list-widget';
-
+import { Period } from '../../../models/period';
 
 @Component({
   selector: 'app-period-candidacy',
-  imports: [FormsModule, NgFor],
+  imports: [FormsModule, NgFor, RouterLink],
   templateUrl: './period-candidacy.component.html',
 })
-export class PeriodCandidacyComponent extends BaseListWidget{
+export class PeriodCandidacyComponent
+  extends BaseListWidget
+  implements OnChanges
+{
   candidacies: Candidacy[] = [];
-  periodId = -1;
+  @Input() period?: Period;
   ville: string = '';
-  route:ActivatedRoute = inject(ActivatedRoute);
+  route: ActivatedRoute = inject(ActivatedRoute);
   candidacyService: CandidacyService = inject(CandidacyService);
   constructor() {
     super();
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['period']) {
+      const current = changes['period'].currentValue;
+      this.period = current;
+      if (current) this.loadData();
+    }
+  }
+
   ngOnInit(): void {
-    this.periodId = Number(this.route.snapshot.paramMap.get('id'));
     this.loadData();
   }
 
   override loadData() {
-    this.candidacyService.getCandidacies(this.currentPage, this.search, this.ville, this.periodId, this.per_page).then(response => {
-      this.candidacies = response.data;
-      console.log("Les candidatures de la periode", response.data);
-      this.currentPage = response.current_page;
-      this.lastPage = response.last_page;
-    });
+    this.candidacyService
+      .getCandidacies(
+        this.currentPage,
+        this.search,
+        this.ville,
+        this.period?.id,
+        this.per_page
+      )
+      .then((response) => {
+        this.candidacies = response.data;
+        this.currentPage = response.current_page;
+        this.lastPage = response.last_page;
+      });
   }
 }
