49de85dbc23096e8256177ccfc974447f271adf7
Dibakala-Muke <dibakala90@gmail.com>
2025-06-09 17:14:15 +0100

    Ajout d'une nouvelle fonctionnalité pour ajouter une période

---
+++ src/app/pages/period/period-modal/period-modal.component.ts
@@ -0,0 +1,44 @@
+import { NgIf } from '@angular/common';
+import { Component, Input, Output, EventEmitter, Inject, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PeriodService } from '../../../services/period.service';
+
+@Component({
+  selector: 'app-period-modal',
+  imports: [ReactiveFormsModule],
+  templateUrl: './period-modal.component.html',
+})
+export class PeriodModalComponent {
+
+  periodService:PeriodService = inject(PeriodService);
+
+
+  constructor(
+    public dialogRef: MatDialogRef<PeriodModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ){
+
+  }
+
+  form = new FormGroup({
+    year: new FormControl(new Date().getFullYear(), [Validators.required])
+  });
+
+  closeModal() {
+    this.dialogRef.close();
+  }
+
+  submit() {
+    if (this.form.valid) {
+      this.createPeriod();
+    }
+  }
+
+  createPeriod() {
+    const year=this.form.value.year as number;
+    this.periodService.createPeriod(year).then(() => {
+      this.dialogRef.close(true);
+    });
+  }
+}
