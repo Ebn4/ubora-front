0ecaaf3cc01884d26cd9fc573953e7d9c5b29961
Dibakala-Muke <dibakala90@gmail.com>
2025-06-09 11:08:22 +0100

    Implémentation detail period

--- src/app/pages/period-single/period-single.component.ts
+++ src/app/pages/period-single/period-single.component.ts
@@ -1,12 +1,14 @@
 import { Period } from './../../models/period';
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { PeriodService } from '../../services/period.service';
 import { NgClass, NgIf } from '@angular/common';
 import { PeriodCriteriaComponent } from './period-criteria/period-criteria.component';
 import { PeriodCandidacyComponent } from './period-candidacy/period-candidacy.component';
 import { PeriodEvaluateurComponent } from './period-evaluateur/period-evaluateur.component';
 import { PeriodLecteurComponent } from './period-lecteur/period-lecteur.component';
+import { MatTabsModule } from "@angular/material/tabs";
+
 
 @Component({
   selector: 'app-period-single',
@@ -18,6 +20,7 @@ import { PeriodLecteurComponent } from './period-lecteur/period-lecteur.componen
     PeriodCandidacyComponent,
     PeriodEvaluateurComponent,
     PeriodLecteurComponent,
+    MatTabsModule
   ],
   templateUrl: './period-single.component.html',
 })
@@ -25,20 +28,65 @@ export class PeriodSingleComponent {
   periodService: PeriodService = inject(PeriodService);
   route: ActivatedRoute = inject(ActivatedRoute);
   period!: Period | undefined;
+  selectedTab: string = "tab1";
+  tabIndex: number = 0;
   periodId = -1;
   showModal = false;
   activeTab: 'criteria' | 'candidacy' | 'evaluateur' | 'lecteur' = 'criteria';
+
+  constructor(private router: Router){}
 
   ngOnInit() {
     this.periodId = Number(this.route.snapshot.paramMap.get('id'));
     this.periodService.getOnePeriod(this.periodId).then((period) => {
       this.period = period;
     });
+    this.route.queryParams.subscribe((params) => {
+      this.selectedTab = params["tab"] || "criteria";
+      this.tabIndex = this.getTabIndex(this.selectedTab);
+    });
   }
 
-  setActiveTab(tab: 'criteria' | 'candidacy' | 'evaluateur' | 'lecteur') {
-    this.activeTab = tab;
+  getTabIndex(tab: string): number {
+    switch (tab) {
+      case "criteria":
+        return 0;
+      case "candidacy":
+        return 1;
+      case "evaluateur":
+        return 2;
+      case "lecteur":
+        return 3;
+      default:
+        return 0;
+    }
   }
+
+  getTabName(index: number): string {
+    switch (index) {
+      case 0:
+        return "criteria";
+      case 1:
+        return "candidacy";
+      case 2:
+        return "evaluateur";
+      case 3:
+        return "lecteur";
+      default:
+        return "criteria";
+    }
+  }
+
+  onTabChange(index: number) {
+    this.selectedTab = this.getTabName(index);
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { tab: this.selectedTab },
+      queryParamsHandling: "merge",
+    });
+  }
+
+
   openModal() {}
 
   closeModal() {
