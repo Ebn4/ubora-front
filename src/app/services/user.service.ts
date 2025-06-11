4f888a8389a7b4a160015d07f1a54d79c337e0b0
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-11 12:25:45 +0100

    feat: creation du service pour gerer les utilisateurs

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,19 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BASE_URL} from '../app.tokens';
+import {ResponseInterface} from '../models/response.model';
+import {LdapUser} from '../models/ldap-user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  http: HttpClient = inject(HttpClient)
+  baseUrl = inject(BASE_URL)
+
+  searchUserFromLdap(query: string) {
+    return this.http.get<ResponseInterface<LdapUser[]>>(`${this.baseUrl}/users/ldap/${query}`)
+  }
+
+}
