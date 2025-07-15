5c2e9e3ed1f93813ecfa402fa0b8e34f8ab2c109
Dibakala-Muke <dibakala90@gmail.com>
2025-07-15 16:20:24 +0100

    Ajout de nouvelles routes pour la gestion de candidats, critères et présélections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
-import {Routes} from '@angular/router';
-import {PeriodComponent} from './pages/period/period.component';
-import {PeriodSingleComponent} from './pages/period-single/period-single.component';
-import {SidebarComponent} from './pages/layout/sidebar/sidebar.component';
-import {LoggedGuard} from './middlewares/logged.guard';
-import {AuthGuard} from './middlewares/auth.guard';
+import { Routes } from '@angular/router';
+import { PeriodComponent } from './pages/period/period.component';
+import { PeriodSingleComponent } from './pages/period-single/period-single.component';
+import { SidebarComponent } from './pages/layout/sidebar/sidebar.component';
+import { PeriodCandidacyComponent } from './pages/period-single/period-candidacy/period-candidacy.component';
+import { CandidacySingleComponent } from './pages/candidacy-single/candidacy-single.component';
+import { CriteriaComponent } from './pages/criteria/criteria.component';
+import { AuthGuard } from './middlewares/auth.guard';
+import { LoggedGuard } from './middlewares/logged.guard';
+import { PreselectionComponent } from './pages/preselection/preselection.component';
+import { CandidacyPreselectionComponent } from './pages/preselection/candidacy-preselection/candidacy-preselection.component';
 
 export const routes: Routes = [
   {
@@ -17,9 +22,14 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
     component: SidebarComponent,
     children: [
-      {path: '', redirectTo: 'period', pathMatch: 'full'},
-      {path: 'period', component: PeriodComponent},
-      {path: 'period-single/:id', component: PeriodSingleComponent},
+      {path: '', redirectTo: 'period', pathMatch:'full'},
+      {path: 'period', component:PeriodComponent},
+      {path: 'period-single/:id', component:PeriodSingleComponent},
+      {path: 'criteria', component:CriteriaComponent},
+      {path: 'candidacy', component:PeriodCandidacyComponent},
+      {path: 'candidacy-single/:id', component:CandidacySingleComponent},
+      {path: 'evaluator-candidacies', component:PreselectionComponent},
+      { path: 'evaluator-candidacies-single/:id/:dispatchId', component: CandidacyPreselectionComponent }
     ]
   }
 ];
