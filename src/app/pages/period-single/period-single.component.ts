847b5fc353f9abb8ae79dcedb0438574f080541a
Dibakala-Muke <dibakala90@gmail.com>
2025-07-15 15:11:57 +0100

    - Remplacement de l’appel à getOnePeriod (Promise) par un appel asynchrone via Observable - Ajout de la récupération de l’année de la période (	his.year). - Appel de modales :   - CriteriaAttachComponent pour attacher des critères de présélection.   - CriteriaAttachSelectionComponent pour attacher des critères de sélection.   - ImportFileCandidaciesComponent pour importer des candidatures. - Ajout d’un menu déroulant avec logique d’ouverture/fermeture (toggleDropdown + HostListener).

--- src/app/pages/period-single/period-single.component.ts
+++ src/app/pages/period-single/period-single.component.ts
@@ -1,72 +1,100 @@
+import { CriteriaAttachComponent } from './criteria-attach/criteria-attach.component';
+import { PeriodCriteriaComponent } from './period-criteria/period-criteria.component';
+
 import { Period } from './../../models/period';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, HostListener } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { PeriodService } from '../../services/period.service';
 import { NgClass, NgIf } from '@angular/common';
-import { PeriodCriteriaComponent } from './period-criteria/period-criteria.component';
 import { PeriodCandidacyComponent } from './period-candidacy/period-candidacy.component';
 import { PeriodEvaluateurComponent } from './period-evaluateur/period-evaluateur.component';
 import { PeriodLecteurComponent } from './period-lecteur/period-lecteur.component';
-import { MatTabsModule } from "@angular/material/tabs";
-import { PeriodStatus } from '../../enum/period-status.enum';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { CriteriaComponent } from '../criteria/criteria.component';
+import { CriteriaAttachSelectionComponent } from './criteria-attach-selection/criteria-attach-selection.component';
+import { ImportFileCandidaciesComponent } from '../import-file-candidacies/import-file-candidacies.component';
 
 @Component({
   selector: 'app-period-single',
   imports: [
     RouterLink,
-    NgIf,
     NgClass,
-    PeriodCriteriaComponent,
     PeriodCandidacyComponent,
     PeriodEvaluateurComponent,
     PeriodLecteurComponent,
-    MatTabsModule
+    MatTabsModule,
+    NgIf,
+    CriteriaComponent,
   ],
   templateUrl: './period-single.component.html',
 })
 export class PeriodSingleComponent {
-  canDispatch = signal(false)
+  canDispatch = signal(false);
   periodService: PeriodService = inject(PeriodService);
   route: ActivatedRoute = inject(ActivatedRoute);
   period!: Period | undefined;
-  selectedTab: string = "tab1";
+  selectedTab: string = 'tab1';
   tabIndex: number = 0;
-  periodId = -1;
+  periodId!: number;
+  year!: number
   showModal = false;
-  activeTab: 'criteria' | 'candidacy' | 'evaluateur' | 'lecteur' = 'criteria';
-  snackBar = inject(MatSnackBar);
-  dispatchPeriodStatus = PeriodStatus.STATUS_DISPATCH
+  isDropdownVisible = false;
+  event = false;
 
+  constructor(private router: Router, private _matDialog: MatDialog) { }
 
-  constructor(private router: Router) {
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+  @HostListener('document:click', ['$event'])
+  closeDropdownOnClick(event: MouseEvent) {
+    const dropdownDefaultButton = document.getElementById(
+      'dropdownDefaultButton'
+    );
+
+    if (
+      dropdownDefaultButton &&
+      !dropdownDefaultButton.contains(event.target as Node)
+    ) {
+      this.isDropdownVisible = false;
+    }
   }
 
   ngOnInit() {
-    this.periodId = Number(this.route.snapshot.paramMap.get('id'));
-    this.getPeriod();
-    this.route.queryParams.subscribe((params) => {
-      this.selectedTab = params["tab"] || "criteria";
-      this.tabIndex = this.getTabIndex(this.selectedTab);
-    });
+    this.loadData()
   }
 
-  getPeriod() {
-    this.periodService.getOnePeriod(this.periodId).then((period) => {
-      this.period = period;
+  loadData() {
+    this.periodId = Number(this.route.snapshot.paramMap.get('id'));
+    this.periodService.getOnePeriod(this.periodId).subscribe({
+      next: (period) => {
+        this.period = period;
+        this.year = this.period.year
+        this.route.queryParams.subscribe((params) => {
+          this.selectedTab = params['tab'] || 'criteria';
+          this.tabIndex = this.getTabIndex(this.selectedTab);
+        });
+      },
+      error: (error) => {
+        console.error('Error fetching period:', error);
+      },
     });
   }
 
   getTabIndex(tab: string): number {
     switch (tab) {
-      case "criteria":
+      case 'criteria':
         return 0;
-      case "candidacy":
+      case 'candidacy':
         return 1;
-      case "evaluateur":
+      case 'evaluateur':
         return 2;
-      case "lecteur":
+      case 'lecteur':
         return 3;
       default:
         return 0;
@@ -76,15 +104,15 @@ export class PeriodSingleComponent {
   getTabName(index: number): string {
     switch (index) {
       case 0:
-        return "criteria";
+        return 'criteria';
       case 1:
-        return "candidacy";
+        return 'candidacy';
       case 2:
-        return "evaluateur";
+        return 'evaluateur';
       case 3:
-        return "lecteur";
+        return 'lecteur';
       default:
-        return "criteria";
+        return 'criteria';
     }
   }
 
@@ -93,37 +121,69 @@ export class PeriodSingleComponent {
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: { tab: this.selectedTab },
-      queryParamsHandling: "merge",
+      queryParamsHandling: 'merge',
     });
   }
 
-
-  openModal() {
-  }
+  openModal() { }
 
   closeModal() {
     this.showModal = false;
   }
 
   canValidateDispatch(canDispatch: boolean) {
-    this.canDispatch.set(canDispatch)
+    this.canDispatch.set(canDispatch);
   }
 
-  validateStatus() {
-    this.periodService
-      .changePeriodStatus(this.periodId, { status: PeriodStatus.STATUS_PRESELECTION })
-      .subscribe({
-        next: value => {
-          this.snackBar.open("Le statut de la période a été mis à jour avec succès.", "Fermer", {
-            duration: 3000,
-          });
-          this.canDispatch.set(false);
-          this.getPeriod();
-        },
-        error: err => {
-          console.log(err);
+  openModalAttach(periodId: number | undefined) {
+    periodId = this.periodId;
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.data = { periodId };
 
-        }
-      })
+    const dialogRef = this._matDialog.open(
+      CriteriaAttachComponent,
+      dialogConfig
+    );
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.ngOnInit();
+      }
+    });
+  }
+  openModalAttachSelection(periodId: number | undefined) {
+    periodId = this.periodId;
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.data = { periodId };
+
+    const dialogRef = this._matDialog.open(
+      CriteriaAttachSelectionComponent,
+      dialogConfig
+    );
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+      }
+    });
+  }
+
+  importFileCandidacies(periodId: number | undefined, year: number | undefined) {
+    periodId = this.periodId;
+    year = this.year;
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.width = '900px';
+    dialogConfig.maxWidth = '100vw';
+    dialogConfig.data = { periodId, year };
+
+    const dialogRef = this._matDialog.open(
+      ImportFileCandidaciesComponent,
+      dialogConfig
+    );
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === true) {
+        this.event = true;
+      }
+    });
   }
 }
