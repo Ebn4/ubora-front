fc3bf32105e18b936cada4a91a8f5cd3fe53e31e
Dibakala-Muke <dibakala90@gmail.com>
2025-07-15 14:36:47 +0100

    Ajout d'un nouveau composant pour afficher les infos des évaluateurs d'un candidat

---
+++ src/app/pages/candidacy-single/candidacy-evaluateurs/candidacy-evaluateurs.component.ts
@@ -0,0 +1,11 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidacy-evaluateurs',
+  imports: [],
+  templateUrl: './candidacy-evaluateurs.component.html',
+})
+export class CandidacyEvaluateursComponent {
+  isopen = false;
+}
