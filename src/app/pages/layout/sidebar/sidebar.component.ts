447e06a163593a34b1b8dd8e96eb50447181a33b
zgeniusecoders <zgeniusecoders@gmail.com>
2025-07-16 16:01:55 +0100

    feat : cacher les menus Candidats, Période, Critère si l'utilisateur connecté n'est pas administrateur

--- src/app/pages/layout/sidebar/sidebar.component.ts
+++ src/app/pages/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
-import { NgClass } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
-import { filter } from 'rxjs';
-import { AuthServices } from '../../../services/auth.service';
-import { LocalStorageService } from '../../../services/local-storage.service';
+import {NgClass} from '@angular/common';
+import {Component, inject, signal} from '@angular/core';
+import {NavigationEnd, Router, RouterLink, RouterOutlet} from '@angular/router';
+import {filter} from 'rxjs';
+import {AuthServices} from '../../../services/auth.service';
+import {LocalStorageService} from '../../../services/local-storage.service';
+import {UserService} from '../../../services/user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,26 +12,44 @@ import { LocalStorageService } from '../../../services/local-storage.service';
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
+
   router: Router = inject(Router);
   authService = inject(AuthServices);
+  userService = inject(UserService);
+
+  hasAdminRole = signal(false)
   localStorageService = inject(LocalStorageService);
 
-  ngOnInit(){
+  ngOnInit() {
     const url = this.router.url;
     this.updateActiveTab(url);
+    this.checkIfUserHasAdminRole()
 
     this.router.events
-    .pipe(filter(event => event instanceof NavigationEnd))
-    .subscribe((event: NavigationEnd) => {
-      this.updateActiveTab((event as NavigationEnd).urlAfterRedirects);
-    })
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.updateActiveTab((event as NavigationEnd).urlAfterRedirects);
+      })
   }
+
   activeTab: 'candidacy' | 'allcandidacy' | 'import' | 'presection' | 'period' | 'criteria' | 'user' | 'evaluator-candidacies' | 'preselection-admin' = 'criteria';
+
   setActiveTab(tab: 'candidacy' | 'allcandidacy' | 'import' | 'presection' | 'period' | 'criteria' | 'user' | 'evaluator-candidacies' | 'preselection-admin') {
     this.activeTab = tab;
   }
 
-  logout(){
+  checkIfUserHasAdminRole() {
+    this.userService.hasAdminRole()
+      .subscribe({
+        next: value => {
+          this.hasAdminRole.set(value.hasAdminRole)
+        }, error: err => {
+          console.error(err)
+        }
+      })
+  }
+
+  logout() {
     this.authService.logout().subscribe({
       next: () => {
         this.localStorageService.removeData('token');
@@ -43,34 +62,25 @@ export class SidebarComponent {
   }
 
   updateActiveTab(url: string) {
-    if(url.includes('period')) {
+    if (url.includes('period')) {
       this.setActiveTab('period');
-    }
-    else if(url.includes('candidacy')) {
+    } else if (url.includes('candidacy')) {
       this.setActiveTab('candidacy');
-    }
-    else if(url.includes('allcandidacy')) {
+    } else if (url.includes('allcandidacy')) {
       this.setActiveTab('allcandidacy');
-    }
-    else if(url.includes('import')) {
+    } else if (url.includes('import')) {
       this.setActiveTab('import');
-    }
-    else if(url.includes('presection')) {
+    } else if (url.includes('presection')) {
       this.setActiveTab('presection');
-    }
-    else if(url.includes('criteria')) {
+    } else if (url.includes('criteria')) {
       this.setActiveTab('criteria');
-    }
-    else if(url.includes('user')) {
+    } else if (url.includes('user')) {
       this.setActiveTab('user');
-    }
-    else if(url.includes('evaluator-candidacies')) {
+    } else if (url.includes('evaluator-candidacies')) {
       this.setActiveTab('evaluator-candidacies');
-    }
-    else if(url.includes('preselection-admin')){
+    } else if (url.includes('preselection-admin')) {
       this.setActiveTab('preselection-admin');
-    }
-    else {
+    } else {
       this.setActiveTab('criteria');
     }
   }
