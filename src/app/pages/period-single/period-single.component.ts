02532ca32ed9b311abed4beeed91158a41551e73
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-04 12:31:22 +0100

    feat: ajout du bouton Cloturer la periode dans le composant period-single.component

--- src/app/pages/period-single/period-single.component.ts
+++ src/app/pages/period-single/period-single.component.ts
@@ -1,27 +1,24 @@
-import { CriteriaAttachComponent } from './criteria-attach/criteria-attach.component';
-import { PeriodCriteriaComponent } from './period-criteria/period-criteria.component';
-
-import { Period } from './../../models/period';
-import { Component, inject, signal, HostListener } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { PeriodService } from '../../services/period.service';
-import { NgClass, NgIf } from '@angular/common';
-import { PeriodCandidacyComponent } from './period-candidacy/period-candidacy.component';
-import { PeriodEvaluateurComponent } from './period-evaluateur/period-evaluateur.component';
-import { PeriodLecteurComponent } from './period-lecteur/period-lecteur.component';
-import { MatTabsModule } from '@angular/material/tabs';
+import {CriteriaAttachComponent} from './criteria-attach/criteria-attach.component';
+import {Period} from './../../models/period';
+import {Component, inject, signal, HostListener} from '@angular/core';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {PeriodService} from '../../services/period.service';
+import {NgClass, NgIf} from '@angular/common';
+import {PeriodCandidacyComponent} from './period-candidacy/period-candidacy.component';
+import {PeriodEvaluateurComponent} from './period-evaluateur/period-evaluateur.component';
+import {MatTabsModule} from '@angular/material/tabs';
 import {
   MatDialog,
   MatDialogConfig,
-  MatDialogRef,
 } from '@angular/material/dialog';
-import { CriteriaComponent } from '../criteria/criteria.component';
-import { CriteriaAttachSelectionComponent } from './criteria-attach-selection/criteria-attach-selection.component';
-import { ImportFileCandidaciesComponent } from '../import-file-candidacies/import-file-candidacies.component';
-import { PeriodStatus } from '../../enum/period-status.enum';
-import { PreselectionService } from '../../services/preselection.service';
-import { ListeningChangeService } from '../../services/listening-change.service';
-import { Subscription } from 'rxjs';
+import {CriteriaComponent} from '../criteria/criteria.component';
+import {CriteriaAttachSelectionComponent} from './criteria-attach-selection/criteria-attach-selection.component';
+import {ImportFileCandidaciesComponent} from '../import-file-candidacies/import-file-candidacies.component';
+import {PeriodStatus} from '../../enum/period-status.enum';
+import {PreselectionService} from '../../services/preselection.service';
+import {ListeningChangeService} from '../../services/listening-change.service';
+import {Subscription} from 'rxjs';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-period-single',
@@ -30,7 +27,6 @@ import { Subscription } from 'rxjs';
     NgClass,
     PeriodCandidacyComponent,
     PeriodEvaluateurComponent,
-    PeriodLecteurComponent,
     MatTabsModule,
     NgIf,
     CriteriaComponent,
@@ -38,11 +34,12 @@ import { Subscription } from 'rxjs';
   templateUrl: './period-single.component.html',
 })
 export class PeriodSingleComponent {
-  validateDispatchPeriodStatus = PeriodStatus.STATUS_DISPATCH
-  canDispatch = signal(false);
+
   periodService: PeriodService = inject(PeriodService);
   preselectionService: PreselectionService = inject(PreselectionService)
   route: ActivatedRoute = inject(ActivatedRoute);
+  readonly snackbar = inject(MatSnackBar)
+
   period!: Period | undefined;
   selectedTab: string = 'tab1';
   tabIndex: number = 0;
@@ -50,14 +47,18 @@ export class PeriodSingleComponent {
   year!: number
   showModal = false;
   isDropdownVisible = false;
+
+  canDispatch = signal(false);
+  validateDispatchPeriodStatus = PeriodStatus.STATUS_DISPATCH
   dispatchStatus = signal(PeriodStatus.STATUS_DISPATCH);
   preselectionStatus = signal(PeriodStatus.STATUS_PRESELECTION);
-  interviewStatus = signal(PeriodStatus.STATUS_INTERVIEW);
+
   private subscription!: Subscription;
 
   event = false;
 
-  constructor(private router: Router, private _matDialog: MatDialog, private modalService: ListeningChangeService) { }
+  constructor(private router: Router, private _matDialog: MatDialog, private modalService: ListeningChangeService) {
+  }
 
   toggleDropdown() {
     this.isDropdownVisible = !this.isDropdownVisible;
@@ -138,12 +139,13 @@ export class PeriodSingleComponent {
     this.selectedTab = this.getTabName(index);
     this.router.navigate([], {
       relativeTo: this.route,
-      queryParams: { tab: this.selectedTab },
+      queryParams: {tab: this.selectedTab},
       queryParamsHandling: 'merge',
     });
   }
 
-  openModal() { }
+  openModal() {
+  }
 
   closeModal() {
     this.showModal = false;
@@ -151,7 +153,8 @@ export class PeriodSingleComponent {
 
   sendEmails() {
     this.preselectionService.sendDispatchNotification().subscribe({
-      next: res => { },
+      next: res => {
+      },
       error: err => console.error(err)
     });
   }
@@ -165,7 +168,7 @@ export class PeriodSingleComponent {
     periodId = this.periodId;
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
-    dialogConfig.data = { periodId };
+    dialogConfig.data = {periodId};
 
     const dialogRef = this._matDialog.open(
       CriteriaAttachComponent,
@@ -177,11 +180,12 @@ export class PeriodSingleComponent {
       }
     });
   }
+
   openModalAttachSelection(periodId: number | undefined) {
     periodId = this.periodId;
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
-    dialogConfig.data = { periodId };
+    dialogConfig.data = {periodId};
 
     const dialogRef = this._matDialog.open(
       CriteriaAttachSelectionComponent,
@@ -200,7 +204,7 @@ export class PeriodSingleComponent {
     dialogConfig.disableClose = true;
     dialogConfig.width = '900px';
     dialogConfig.maxWidth = '100vw';
-    dialogConfig.data = { periodId, year };
+    dialogConfig.data = {periodId, year};
 
     const dialogRef = this._matDialog.open(
       ImportFileCandidaciesComponent,
@@ -215,7 +219,7 @@ export class PeriodSingleComponent {
 
   validateDispatch() {
     this.periodService
-      .changePeriodStatus(this.periodId, { status: PeriodStatus.STATUS_PRESELECTION })
+      .changePeriodStatus(this.periodId, {status: PeriodStatus.STATUS_PRESELECTION})
       .subscribe({
         next: value => {
           this.loadData()
@@ -226,4 +230,22 @@ export class PeriodSingleComponent {
         }
       })
   }
+
+  closeStatus() {
+    if (this.period) {
+      this.periodService.changePeriodStatus(this.period.id, {status: PeriodStatus.STATUS_CLOSE})
+        .subscribe({
+          next: res => {
+            this.loadData()
+          },
+          error: err => {
+            this.snackbar.open(err.error.errors, 'Fermer', {
+              duration: 3000,
+            });
+          }
+        })
+    }
+  }
+
+  protected readonly PeriodStatus = PeriodStatus;
 }
