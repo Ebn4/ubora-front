1314106f064c2bea1f4f51998b9003c680fbeefc
Dibakala-Muke <dibakala90@gmail.com>
2025-08-07 18:05:45 +0100

    Ajout de la méthode getOnePeriod() de PeriodService dans CandidacySingleComponent

--- src/app/pages/candidacy-single/candidacy-single.component.ts
+++ src/app/pages/candidacy-single/candidacy-single.component.ts
@@ -4,25 +4,39 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { NgClass } from '@angular/common';
 import { CandidacyInformationsComponent } from "./candidacy-informations/candidacy-informations.component";
 import { CandidacyEvaluateursComponent } from "./candidacy-evaluateurs/candidacy-evaluateurs.component";
+import { PeriodService } from '../../services/period.service';
+import { Period } from '../../models/period';
 
 @Component({
   selector: 'app-candidacy-single',
-  imports: [MatTabsModule, NgClass, CandidacyInformationsComponent, CandidacyEvaluateursComponent],
+  imports: [MatTabsModule, NgClass, CandidacyInformationsComponent, CandidacyEvaluateursComponent, RouterLink],
   templateUrl: './candidacy-single.component.html',
 })
 export class CandidacySingleComponent {
+  periodId!: number;
+  period!: Period
+  periodService: PeriodService = inject(PeriodService)
   selectedTab: string = "tab1";
   tabIndex: number = 0;
   route: ActivatedRoute = inject(ActivatedRoute);
+  showModal = false;
 
-  constructor(private router: Router){
+  constructor(private router: Router) {
 
   }
   ngOnInit() {
+    this.periodId = Number(this.route.snapshot.paramMap.get('period_id'));
     this.route.queryParams.subscribe((params) => {
       this.selectedTab = params["tab"] || "information";
       this.tabIndex = this.getTabIndex(this.selectedTab);
     });
+
+    this.periodService.getOnePeriod(this.periodId).subscribe({
+      next: (response) => {
+        this.period = response
+      },
+      error: (error) => { console.log("error : ", error) }
+    })
   }
 
   getTabIndex(tab: string): number {
