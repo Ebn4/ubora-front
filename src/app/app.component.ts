f18e05b33dc9fb17df349eac771f2a2dd5b6a442
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-13 11:15:25 +0100

    feat: integration du composant mat-progress-bar dans app.component pour afficher la barre de progression pour pour chaque requete http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { initFlowbite } from 'flowbite';
+import {Component, inject} from '@angular/core';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet} from '@angular/router';
+import {initFlowbite} from 'flowbite';
+import {LoadingService} from './services/loading.service';
+import {Subscription} from 'rxjs';
+import {MatProgressBar} from '@angular/material/progress-bar';
+import {AsyncPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatProgressBar, NgIf, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
+  private sub = new Subscription();
+  router = inject(Router)
+  loading = inject(LoadingService)
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
   ngOnInit(): void {
     //alert('Hello World!')
     initFlowbite();
+    this.sub.add(
+      this.router.events.subscribe(e => {
+        if (e instanceof NavigationStart) this.loading.show();
+        if (e instanceof NavigationEnd || e instanceof NavigationCancel || e instanceof NavigationError) this.loading.hide();
+      })
+    );
   }
 }
