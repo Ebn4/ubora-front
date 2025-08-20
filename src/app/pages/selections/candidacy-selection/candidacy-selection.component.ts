505ea6f4caa828453ec0a9f5e365fc5854bc3a05
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-20 15:41:58 +0100

    feat: ajout de la recherche dans la page candidacy-selection.component

--- src/app/pages/selections/candidacy-selection/candidacy-selection.component.ts
+++ src/app/pages/selections/candidacy-selection/candidacy-selection.component.ts
@@ -1,13 +1,10 @@
 import {Component, inject, signal} from '@angular/core';
 import {Candidacy} from '../../../models/candidacy';
 import {CandidacyService} from '../../../services/candidacy.service';
-import {ActivatedRoute, Router, RouterLink} from '@angular/router';
-import {CriteriaPeriod} from '../../../models/criteria-period';
-import {CriteriaService} from '../../../services/criteria.service';
+import {ActivatedRoute, RouterLink} from '@angular/router';
 import {PeriodStatus} from '../../../enum/period-status.enum';
 import {ImportService} from '../../../services/import.service';
-import {NgClass, NgFor, NgIf} from '@angular/common';
-import {PreselectionService} from '../../../services/preselection.service';
+import {AsyncPipe} from '@angular/common';
 import {Interview} from '../../../models/interview';
 import {Period} from '../../../models/period';
 import {EvaluationComponent} from '../../evaluation/evaluation.component';
@@ -15,16 +12,21 @@ import {PeriodService} from '../../../services/period.service';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {DocPreviewComponent} from '../../preselection/candidacy-preselection/doc-preview/doc-preview.component';
 import {FilePreviewService} from '../../../services/file-preview.service';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from '@angular/material/autocomplete';
+import {MatInput} from '@angular/material/input';
+import {Observable, of} from 'rxjs';
 
 @Component({
   selector: 'app-candidacy-preselection',
-  imports: [RouterLink, EvaluationComponent],
+  imports: [RouterLink, EvaluationComponent, AsyncPipe, FormsModule, MatAutocomplete, MatAutocompleteTrigger, MatInput, MatOption, ReactiveFormsModule],
   templateUrl: './candidacy-selection.component.html',
 })
 export class CandidacySelectionComponent {
 
   constructor(private _matDialog: MatDialog) {
   }
+
   periodService = inject(PeriodService);
   filePreviewService = inject(FilePreviewService)
   candidacyService: CandidacyService = inject(CandidacyService);
@@ -38,10 +40,13 @@ export class CandidacySelectionComponent {
 
   candidacy?: Candidacy;
   currentIndex: number = 0;
+  query = new FormControl('')
+  filterCandidates: Observable<Candidacy[]> | undefined;
 
   candidateHasSelected = signal(true)
   candidateId = signal(0)
   interview = signal<Interview | null>(null)
+  candidates = signal<Candidacy[]>([])
   period = signal<Period | null>(null)
 
   ngOnInit() {
@@ -50,6 +55,10 @@ export class CandidacySelectionComponent {
     this.loadDataCandidacy(currentId)
     this.loadCandidateInterviewInfo()
     this.checkIfCandidateHasSelected()
+
+    this.query.valueChanges.subscribe(value => {
+      this.onSearchCandidate(value ?? '')
+    });
   }
 
   loadDataCandidacy(candidacyId: number) {
@@ -115,14 +124,14 @@ export class CandidacySelectionComponent {
     const actualFileName = fileName;
 
     this.importService.getDocument(actualFileName).subscribe((file) => {
-      const blob = new Blob([file], { type: file.type });
-      const fileFromUrl = new File([blob], "test-doc.docx", { type: blob.type });
+      const blob = new Blob([file], {type: file.type});
+      const fileFromUrl = new File([blob], "test-doc.docx", {type: blob.type});
 
       this.filePreviewService.previewFile(fileFromUrl).subscribe({
         next: (result) => {
           const dialogConfig = new MatDialogConfig();
           dialogConfig.disableClose = true;
-          dialogConfig.data = { currentPreview: result };
+          dialogConfig.data = {currentPreview: result};
 
           const dialogRef = this._matDialog.open(DocPreviewComponent, dialogConfig);
 
@@ -136,6 +145,29 @@ export class CandidacySelectionComponent {
         }
       });
     });
+  }
+
+  onSearchCandidate(query: string) {
+    if (query != "") {
+      this.candidacyService.getPreselectedCandidates(1, query, 10).subscribe({
+        next: value => {
+          console.log(value)
+          this.candidates.set(value.data)
+          this.filterCandidates = of(value.data)
+        },
+        error: err => {
+          console.log(err)
+        }
+      })
+    }
+  }
+
+  displayFn(candidacy: Candidacy): string {
+    return candidacy && candidacy.etn_nom ? candidacy.etn_prenom : '';
+  }
+
+  onSelectedCandidate(candidate: Candidacy) {
+    this.candidacy = candidate
   }
 
   protected readonly PeriodStatus = PeriodStatus;
